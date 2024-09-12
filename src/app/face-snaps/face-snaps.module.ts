930be1f10551b83c569560b0e2eb7a09f6850f29
SaidouBalde05 <baldeemamadousaidou20@gmail.com>
2024-09-12 19:51:41 +0000

    module

--- src/app/face-snaps/face-snaps.module.ts
+++ src/app/face-snaps/face-snaps.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { FaceSnapListComponent } from './components/face-snap-list/face-snap-list.component';
+import { SingleFaceSnapComponent } from './components/single-face-snap/single-face-snap.component';
+import { NewFaceSnapComponent } from './components/new-face-snap/new-face-snap.component';
 
-
+const routes: Routes = [
+  { path: 'create', component: NewFaceSnapComponent },
+  { path: '', component: FaceSnapListComponent },
+  { path: ':id', component: SingleFaceSnapComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+  ],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class FaceSnapsModule { }
+export class FaceSnapsModule {}
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,12 +1,27 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule
+//   ]
+// })
+// export class LandingPageModule { }
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
-
+const routes: Routes = [
+  { path: '', component: LandingPageComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+  ],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
